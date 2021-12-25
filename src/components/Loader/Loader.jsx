29ebeecccc14d66d49d92b59dda40d4f836141d9
Vlad <vvllaadd1233345@gmail.com>
2021-12-25 14:30:12 +0200

    fix(loader (component)): delete simecolon after spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import { Container } from './Lodaer.styled';
 export default function Loader(props) {
   return (
     <Container>
-      <Spinner type="Puff" color="#00BFFF" height={100} width={100} />;
+      <Spinner type="Puff" color="#00BFFF" height={100} width={100} />
     </Container>
   );
 }
