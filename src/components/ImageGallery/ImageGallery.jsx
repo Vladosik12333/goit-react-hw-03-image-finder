026411001f6031361ad81ce7bcb2c6b3d67e38e7
Vlad <vvllaadd1233345@gmail.com>
2022-01-02 19:21:18 +0200

    fix(spinner, imagegallery components): fix all notes

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,6 +4,7 @@ import ImageGalleryItem from 'components/ImageGalleryItem';
 import Button from 'components/Button';
 import { List } from './ImageGallery.styled';
 import propTypes from 'prop-types';
+import Spinner from 'components/Spinner';
 
 export default class ImageGallery extends Component {
   static propTypes = {
@@ -18,42 +19,48 @@ export default class ImageGallery extends Component {
     page: 1,
   };
 
-  componentDidUpdate(prevProps) {
-    const prevResp = prevProps.search;
-    const nextResp = this.props.search;
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevProps.search;
+    const nextSearch = this.props.search;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
 
-    if (prevResp !== nextResp) {
+    if (prevSearch !== nextSearch) {
       this.setState({ page: 1, images: [] });
-      this.fetchImages(1);
     }
-    this.scrollToBottom();
+
+    if (
+      (prevSearch !== nextSearch && nextPage === 1) ||
+      prevPage !== nextPage
+    ) {
+      this.setState({
+        status: 'pending',
+      });
+      api(nextSearch, this.state.page)
+        .then(resp => {
+          this.setState(state => {
+            return {
+              images: [...state.images, ...resp.hits],
+              status: 'resolved',
+            };
+          });
+          this.scrollToBottom();
+        })
+        .catch(error => {
+          this.setState({
+            error: error.message,
+            status: 'rejected',
+            page: 1,
+            images: [],
+          });
+        });
+    }
   }
 
   onClickButton = () => {
-    this.fetchImages(this.state.page);
-  };
-
-  fetchImages = page => {
-    const nextResp = this.props.search;
-    api(nextResp, page).then(resp => {
-      if (typeof resp !== 'string') {
-        this.setState(state => {
-          return {
-            images: [...state.images, ...resp.hits],
-            status: 'resolved',
-            page: state.page + 1,
-          };
-        });
-        return;
-      }
-
-      this.setState({
-        error: resp,
-        status: 'rejected',
-        page: 1,
-        images: [],
-      });
-    });
+    this.setState(state => ({
+      page: state.page + 1,
+    }));
   };
 
   scrollToBottom = () => {
@@ -72,26 +79,23 @@ export default class ImageGallery extends Component {
 
     return (
       <>
-        {images.length !== 0 ? (
-          <>
-            <List>
-              {images.map(({ id, webformatURL, tags, largeImageURL }) => {
-                return (
-                  <ImageGalleryItem
-                    key={id}
-                    url={webformatURL}
-                    tags={tags}
-                    onClickToModal={this.props.onClickToModal}
-                    largeImageURL={largeImageURL}
-                  />
-                );
-              })}
-            </List>
-            <Button onClickButton={this.onClickButton} />
-          </>
-        ) : (
-          <></>
+        {images.length !== 0 && (
+          <List>
+            {images.map(({ id, webformatURL, tags, largeImageURL }) => {
+              return (
+                <ImageGalleryItem
+                  key={id}
+                  url={webformatURL}
+                  tags={tags}
+                  onClickToModal={this.props.onClickToModal}
+                  largeImageURL={largeImageURL}
+                />
+              );
+            })}
+          </List>
         )}
+        {status === 'pending' && <Spinner />}
+        {status === 'resolved' && <Button onClickButton={this.onClickButton} />}
       </>
     );
   }
